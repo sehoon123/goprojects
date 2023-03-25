a840a7ae12555fc0c73829f37cfecf028c7f6101
sehoon <sehoon0519@naver.com>
2023-03-26 00:05:55 +0900

    dp

--- baekjoon/2133/2133.go
+++ baekjoon/2133/2133.go
@@ -9,7 +9,6 @@ import (
 var (
 	sc = bufio.NewScanner(os.Stdin)
 	wr = bufio.NewWriter(os.Stdout)
-	dp []int
 )
 
 func nextInt() int {
@@ -27,13 +26,12 @@ func main() {
 	defer wr.Flush()
 
 	n := nextInt()
-	dp = make([]int, n+1)
-	dp[1] = 0
+	dp := make([]int, n+2)
 	dp[2] = 3
 
-	for i := 4; i <= n; i += 2 {
+	for i := 4; i < n+1; i += 2 {
 		dp[i] = dp[i-2] * dp[2]
-		for j := 4; j <= i; j += 2 {
+		for j := 4; j < i; j += 2 {
 			dp[i] += dp[i-j] * 2 // 2는 짝수일때 생기는 특이한 모양에 대응하는 숫자임
 		}
 		dp[i] += 2
