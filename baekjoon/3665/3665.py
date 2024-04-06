f42de0e3ca8380641a5ccadf7d7b4818a72368b3
SH Jung <60769447+sehoon123@users.noreply.github.com>
2024-04-07 01:03:49 +0900

    day 8, topology sort

--- baekjoon/3665/3665.py
+++ baekjoon/3665/3665.py
@@ -1,22 +1,65 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-T = int(input())
-
-for _ in range(T):
+def read_input():
     n = int(input())
-    indegree = [0] * (n+1)
-    graph = [[] for _ in range(n+1)]
-    tmp = list(map(int,input().split()))
+    graph = [[] for _ in range(n + 1)]
+    in_degree = [0 for _ in range(n + 1)]
+    team = list(map(int, input().split()))
 
-    for i in range(n-1):
-        graph[tmp[i]].append(tmp[i+1])
-        indegree[tmp[i+1]] += 1
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            graph[team[i]].append(team[j])
+            in_degree[team[j]] += 1
 
     m = int(input())
     for _ in range(m):
-        a, b = map(int, input().split())
-    
-    print(graph)
-    print(indegree)
+        first, second = map(int, input().split())
+        if second in graph[first]:
+            graph[first].remove(second)
+            in_degree[second] -= 1
+            graph[second].append(first)
+            in_degree[first] += 1
+        else:
+            graph[second].remove(first)
+            in_degree[first] -= 1
+            graph[first].append(second)
+            in_degree[second] += 1
+
+    return n, graph, in_degree
+
+def topological_sort(n, graph, in_degree):
+    queue = []
+    answer = []
+
+    for i in range(1, n + 1):
+        if in_degree[i] == 0:
+            queue.append(i)
+
+    if not queue:
+        return "IMPOSSIBLE"
+
+    while queue:
+        if len(queue) > 1:
+            return "IMPOSSIBLE"
+
+        node = queue.pop()
+        answer.append(node)
+        for neighbor in graph[node]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+            elif in_degree[neighbor] < 0:
+                return "IMPOSSIBLE"
+
+    if len(answer) < n:
+        return "IMPOSSIBLE"
+    else:
+        return " ".join(map(str, answer))
+
+t = int(input())
+
+for _ in range(t):
+    n, graph, in_degree = read_input()
+    result = topological_sort(n, graph, in_degree)
+    print(result)
